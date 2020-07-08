078272478277a485b08a780c07fcab6eef1dfd6e
david prada linarez <davidprali27@gmail.com>
2020-07-08 10:24:29 -0500

    my app

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 const config = {
   api: {
-    port: process.env.PORT || 3000,
     uri: process.env.DB_MONGO || "mongodb://localhost/api-covid",
-    host: process.env.HOST || "https://vibbeapp.herokuapp.com",
+    host: process.env.HOST || "http://localhost:4000",
   },
 };
 
